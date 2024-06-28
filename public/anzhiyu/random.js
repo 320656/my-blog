c55b86951183de37a0bc9748dab8964098527792
w3ngjin <w320656@163.com>
2024-06-28 22:06:13 +0800

    更新

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/27/Hexo-入门指南/","2024/06/27/hello-world/"];function toRandomPost(){
+var posts=["2024/06/27/hello-world/","2024/06/27/Hexo-入门指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
