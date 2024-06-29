65bf9ee8d1abba5efa5749314cbcea685eeea5a0
w3ngjin <w320656@163.com>
2024-06-30 01:37:18 +0800

    Site updated: YYYY-06-2024年6月30日 01:37:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/29/GithubPages自定义域名/","2024/06/27/Hexo-入门指南/","2024/06/29/Macbook终端处理文本/","2024/06/27/hello-world/","2024/06/28/国内服务器失败教训/","2024/06/29/多重部署测试/"];function toRandomPost(){
+var posts=["2024/06/29/GithubPages自定义域名/","2024/06/27/Hexo-入门指南/","2024/06/28/腾讯云建站失败/","2024/06/27/hello-world/","2024/06/29/多重部署测试/","2024/06/29/Macbook终端处理文本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
